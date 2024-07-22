bdbbf1b8d4276342c3108f0810737c0914d1bb51
NohJin5361 <149451923+NohJin5361@users.noreply.github.com>
2024-07-22 14:46:57 +0900

    Add files via upload

---
+++ map_data_project/insertDots.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql');
+
+// MySQL 데이터베이스 연결 설정
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: '330632567', 
+  user: 'root',  // MySQL 사용자 이름
+  password: '1234',  // MySQL 비밀번호
+  database: 'map_data'  // 사용할 데이터베이스 이름
+});
+
+connection.connect();
+
+// dots 배열 예제
+const dots = [
+  {circle: {latitude: 33.450701, longitude: 126.570667}, distance: null},
+  {circle: {latitude: 33.451701, longitude: 126.571667}, distance: 100}
+];
+
+// dots 배열의 데이터를 MySQL 테이블에 삽입하는 함수
+function insertDotsData(dots) {
+  dots.forEach
+}

97fe148322f081e200a25f5d46b06413f40d8edd
NohJin5361 <149451923+NohJin5361@users.noreply.github.com>
2024-07-23 15:51:16 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const app = express();
-const port = 32567
+const port = 3000
 require('dotenv').config()
 
 app.use(bodyParser.urlencoded({ extended: true }));

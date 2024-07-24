931734d3201ae1d2a09f74885e0b8826271ad75b
NohJin5361 <149451923+NohJin5361@users.noreply.github.com>
2024-07-24 10:48:28 +0900

    Add files via upload

---
+++ MyWay-develop/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', './frontend/views');
+app.use(express.static('public'));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+});
+
+// 라우터 가져오기
+const mapRoutes = require('./frontend/routes/mapRoutes'); 
+const pathRoutes = require('./backend/routes/pathRoutes');
+
+// 라우터 사용
+app.use('/', mapRoutes); 
+app.use('/api/paths', pathRoutes);
+

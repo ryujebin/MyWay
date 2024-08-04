387a8d6b55928e5f89038b34e64b81956016fa88
NohJin5361 <149451923+NohJin5361@users.noreply.github.com>
2024-08-05 03:04:53 +0900

    Delete backend directory

--- backend/src/routes/pathRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const pathController = require('../controllers/pathContoller');  
-
-router.post('/save_path', pathController.savePath);
-router.get('/load_paths', pathController.loadPaths);
-
-module.exports = router;

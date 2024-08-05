409a9a83f32eb86c80f38a8342852d6ed7be21fb
PLT <rkdus9910@naver.com>
2024-08-05 21:11:10 +0900

    즐겨찾기 수정

--- frontend/src/components/WalkTrail.js
+++ frontend/src/components/WalkTrail.js
@@ -48,15 +48,9 @@ function WalkTrail() {
                 <button className="setting_button">삭제</button>
               </div>
             </div>
-          </section>
-        </section>
-
-        <section className="walk_trail">
-          <div className="box1">즐겨찾기한 산책로 목록</div>
-          <section className="post">
             <div className="box2">
               <div className="walk_trail_image">
-                <img src="/img/liked_trail_image1.jpg" style={{ width: '100px', margin: '10px' }} alt="Walk Trail 1" />
+                <img src="/img/liked_trail_image1.jpg" style={{ width: '100px', margin: '10px' }} alt="Walk Trail 3" />
               </div>
               <div className="walk_trail_body">
                 <h1>국가정원</h1>
@@ -70,7 +64,7 @@ function WalkTrail() {
             </div>
             <div className="box2">
               <div className="walk_trail_image">
-                <img src="/img/liked_trail_image2.jpg" style={{ width: '100px', margin: '10px' }} alt="Walk Trail 2" />
+                <img src="/img/liked_trail_image2.jpg" style={{ width: '100px', margin: '10px' }} alt="Walk Trail 4" />
               </div>
               <div className="walk_trail_body">
                 <h1>워크홀릭의 알쓸산잡1</h1>
@@ -83,25 +77,11 @@ function WalkTrail() {
               </div>
             </div>
             <div className="box2">
-              <div className="walk_trail_image">
-                <img src="/img/liked_trail_image2.jpg" style={{ width: '100px', margin: '10px' }} alt="Walk Trail 2" />
-              </div>
-              <div className="walk_trail_body">
-                <h1>워크홀릭의 알쓸산잡7</h1>
-                <p>알아두면 쓸데있는 산책로 잡지식</p>
-                <p>#강가산책 #반려견 산책</p>
-              </div>
-              <div className="setting_button_container">
-                <button className="setting_button">수정</button>
-                <button className="setting_button">삭제</button>
-              </div>
-            </div>
-            <div className="box2">
               <div className="walk_trail_i/images/liked_trail_image1.jpgmage">
-                <img src="/img/liked_trail_image3.jpg" style={{ width: '100px', margin: '10px' }} alt="Walk Trail 1" />
+                <img src="/img/liked_trail_image3.jpg" style={{ width: '100px', margin: '10px' }} alt="Walk Trail 6" />
               </div>
               <div className="walk_trail_body">
-                <h1>워크홀릭의 알쓸산잡7</h1>
+                <h1>워크홀릭의 알쓸산잡2</h1>
                 <p>알아두면 쓸데있는 산책로 잡지식</p>
                 <p>#낭만가득 #인생사진</p>
               </div>
@@ -113,7 +93,7 @@ function WalkTrail() {
           </section>
         </section>
         <div className="register_button_container"> 
-          <Link to="/newwalktrail" className="button-box">산책로 등록 바로가기</Link>
+          <Link to="/map" className="button-box">산책로 등록 바로가기</Link>
         </div>
       </main>
     </div>
